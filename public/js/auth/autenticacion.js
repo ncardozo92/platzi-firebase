fee803d6887db05302317dc2690df0d435d0e581
Nicolas Cardozo <Nicolas.Cardozo@ar.ey.com>
2022-10-06 11:24:20 -0300

    commit inicial

---
+++ public/js/auth/autenticacion.js
@@ -0,0 +1,58 @@
+class Autenticacion {
+  autEmailPass (email, password) {
+    //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
+    //Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
+    //$('.modal').modal('close')
+   
+  }
+
+  crearCuentaEmailPass (email, password, nombres) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        result.user.updateProfile({
+          "displayName": nombres
+        })
+
+        const config = {
+          url: "localhost:3000"
+        }
+      
+        result.user.sendEmailVerification(config).catch(error => {
+          console.error(error);
+          Materialize.Toast(error.message, 4000);
+        });
+
+        firebase.auth().signOut();
+
+        Materialize.toast(
+          `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
+          4000
+        )
+    
+        $('.modal').modal('close')
+
+      })
+      .catch(error => {
+        console.error(error);
+        Materialize.Toast(error.message, 4000);
+      });
+    
+    
+  }
+
+  authCuentaGoogle () {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authCuentaFacebook () {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authTwitter () {
+    // TODO: Crear auth con twitter
+  }
+}
---
+++ public/js/general.js
@@ -0,0 +1,53 @@
+$(() => {
+  $('.tooltipped').tooltip({ delay: 50 })
+  $('.modal').modal()
+
+  // TODO: Adicionar el service worker
+
+  // Init Firebase nuevamente
+  // cambio nombre del parámetro de config a varConfig
+  //NICO: esto va a romper, solo hay que invocar initializeApp que ya se importa en el index
+  firebase.initializeApp(varConfig);
+
+  // TODO: Registrar LLave publica de messaging
+
+  // TODO: Solicitar permisos para las notificaciones
+
+  // TODO: Recibir las notificaciones cuando el usuario esta foreground
+
+  // TODO: Recibir las notificaciones cuando el usuario esta background
+
+  // TODO: Listening real time
+
+  // TODO: Firebase observador del cambio de estado
+  //$('#btnInicioSesion').text('Salir')
+  //$('#avatar').attr('src', user.photoURL)
+  //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
+  //$('#btnInicioSesion').text('Iniciar Sesión')
+  //$('#avatar').attr('src', 'imagenes/usuario.png')
+
+  // TODO: Evento boton inicio sesion
+  $('#btnInicioSesion').click(() => {
+    //$('#avatar').attr('src', 'imagenes/usuario.png')
+    // Materialize.toast(`Error al realizar SignOut => ${error}`, 4000)
+    
+
+    $('#emailSesion').val('')
+    $('#passwordSesion').val('')
+    $('#modalSesion').modal('open')
+  })
+
+  $('#avatar').click(() => {
+    //$('#avatar').attr('src', 'imagenes/usuario.png')
+    //Materialize.toast(`SignOut correcto`, 4000)
+  })
+
+  $('#btnTodoPost').click(() => {
+    $('#tituloPost').text('Posts de la Comunidad')   
+  })
+
+  $('#btnMisPost').click(() => {
+    //$('#tituloPost').text('Mis Posts')
+    //Materialize.toast(`Debes estar autenticado para ver tus posts`, 4000)    
+  })
+})
